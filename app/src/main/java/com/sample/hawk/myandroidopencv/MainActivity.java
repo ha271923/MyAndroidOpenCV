a71cac915f0a601b2c342505bad69318ca5de8c2
hawk_wei <hawk_wei@htc.com>
2018-03-13 17:01:26 +0800

    Sample: Apply OpenCV edge effect on a picture.

--- app/src/main/java/com/sample/hawk/myandroidopencv/MainActivity.java
+++ app/src/main/java/com/sample/hawk/myandroidopencv/MainActivity.java
@@ -1,10 +1,18 @@
 package com.sample.hawk.myandroidopencv;
 
-import android.support.v7.app.AppCompatActivity;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    ImageView imgView;
+    Button btnNDK, btnRestore;
 
     // Used to load the 'native-lib' library on application startup.
     static {
@@ -19,9 +27,41 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        // Example of a call to a native method
+        btnRestore = (Button) this.findViewById(R.id.btnRestore);
+        btnRestore.setOnClickListener(new ClickEvent());
+        btnNDK = (Button) this.findViewById(R.id.btnNDK);
+        btnNDK.setOnClickListener(new ClickEvent());
+        imgView = (ImageView) this.findViewById(R.id.ImageView01);
+        Bitmap img = ((BitmapDrawable) getResources().getDrawable(
+                R.drawable.girl)).getBitmap();
+        imgView.setImageBitmap(img);
+// Example of a call to a native method
         TextView tv = (TextView) findViewById(R.id.sample_text);
         tv.setText(stringFromJNI() + '\n' + getTestStringFromJNI());
+    }
+
+    class ClickEvent implements View.OnClickListener {
+        public void onClick(View v) {
+//btnRestore.setText(ImgFun());
+            if (v == btnNDK) {
+                long current = System.currentTimeMillis();
+                Bitmap img1 = ((BitmapDrawable) getResources().getDrawable(
+                        R.drawable.girl)).getBitmap();
+                int w = img1.getWidth(), h = img1.getHeight();
+                int[] pix = new int[w * h];
+                img1.getPixels(pix, 0, w, 0, 0, w, h);
+                int[] resultInt = getCannyImg(pix, w, h);
+                Bitmap resultImg = Bitmap.createBitmap(w, h, Bitmap.Config.RGB_565);
+                resultImg.setPixels(resultInt, 0, w, 0, 0, w, h);
+                long performance = System.currentTimeMillis() - current;
+                imgView.setImageBitmap(resultImg);
+            } else if (v == btnRestore) {
+              Bitmap img2 = ((BitmapDrawable) getResources().getDrawable(
+                      R.drawable.girl)).getBitmap();
+              imgView.setImageBitmap(img2);
+            }
+
+        }
     }
 
     /**
